d98d6416ca14239055c21b7bfabd50aad178ecad
Saekon <s344194@oslomet.no>
2021-09-29 15:44:38 +0200

    Lagt til kode for valg av reise, og kjøp av billetter

--- WebApplication1/wwwroot/js/index.js
+++ WebApplication1/wwwroot/js/index.js
@@ -1,27 +1,69 @@
-﻿$(function () {
-    hentAlleReiser();
+﻿$(document).ready(function () {
+    hentAlleReiser("");
+
+    /*
+    $("#valgKiel").click(function () {
+        hentAlleReiser("Kiel");
+    })
+
+    $("#valgKobenhavn").click(function () {
+        hentAlleReiser("København");
+    })
+
+    $("#visAlle").click(function () {
+        hentAlleReiser("");
+    })
+    */
+
+    $("#reiseValg").change(function () {
+        var value = $(this).val();
+
+        if (value == "Kiel") {
+            hentAlleReiser("Kiel");
+        };
+        if (value == "Kobenhavn") {
+            hentAlleReiser("København");
+        };
+        if (value == "" || value == "visAlle") {
+            hentAlleReiser("");
+        }
+    })
 });
 
-function hentAlleReiser() {
+function hentAlleReiser(dest) {
     $.get("reise/hentAlle", function (reiser) {
-        formaterReiser(reiser);
+        formaterReiser(reiser, dest);
     });
 }
 
-function formaterReiser(reiser) {
+function formaterReiser(reiser, dest) {
     let ut = "<table class='table table-striped'>" +
         "<tr>" +
-        "<th>Reise-id</th><th>Reises fra</th><th>Reises til</th><th>Avgang</th><th>Ankomst</th>" +
+        "<th>Reise-id</th><th>Kjøp billett</th><th>Reises til</th><th>Avgang</th><th>Ankomst</th>" +
         "</tr>";
 
-    for (let reise of reiser) {
-        ut += "<tr>" +
-            "<td>" + reise.id + "</td>" +
-            "<td>" + reise.reiseTil + "</td>" +
-            "<td>" + reise.reiseFra + "</td>" +
-            "<td>" + reise.tidspunktFra + "</td>" +
-            "<td>" + reise.tidspunktTil + "</td>" +
-            "</tr>";
+    if (dest == "") {
+        for (let reise of reiser) {
+            ut += "<tr>" +
+                "<td><button id=" + reise.id + ">Kjøp her</button></td>" +
+                "<td>" + reise.reiseTil + "</td>" +
+                "<td>" + reise.reiseFra + "</td>" +
+                "<td>" + reise.tidspunktFra + "</td>" +
+                "<td>" + reise.tidspunktTil + "</td>" +
+                "</tr>";
+        }
+    } else {
+        for (let reise of reiser) {
+            if (reise.reiseTil == dest) {
+                ut += "<tr>" +
+                    "<td><button id=" + reise.id + ">Kjøp her</button></td>" +
+                    "<td>" + reise.reiseTil + "</td>" +
+                    "<td>" + reise.reiseFra + "</td>" +
+                    "<td>" + reise.tidspunktFra + "</td>" +
+                    "<td>" + reise.tidspunktTil + "</td>" +
+                    "</tr>";
+            }
+        }
     }
     ut += "</table>";
     $("#reisene").html(ut);
