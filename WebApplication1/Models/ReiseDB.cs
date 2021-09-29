77e6858cfbad2c12b82bd01c7378e4395a2b3d76
AndreasTHansen <andreas-torres@hotmail.com>
2021-09-29 12:38:38 +0200

    Endret navn til ReiseDB, og lagt til reiser

---
+++ WebApplication1/Models/ReiseDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ReiseDB : DbContext
+    {
+        public class ReiseDB : DbContext
+        {
+            public ReiseDB(DbContextOptions<ReiseDB> options) : base(options)
+            {
+                Database.EnsureCreated();
+            }
+
+            public DbSet<Reise> Reiser { get; set; }
+        }
+    }
+}
