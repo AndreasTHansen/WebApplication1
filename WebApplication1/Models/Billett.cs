cedf968a1e6996a7c6f62f8a67ee29273aaee333
PandersPanda <anderh0709@gmail.com>
2021-10-04 13:18:21 +0200

    Lagt til regex på klassene, disse er opp for endring

--- WebApplication1/Models/Billett.cs
+++ WebApplication1/Models/Billett.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Modules;
@@ -9,18 +10,28 @@ namespace WebApplication1.Models
     public class Billett
     {
         public int id { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20})$")]
         public string fornavn { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20})$")]
         public string etternavn { get; set; }
+        [RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ. \-@]{2,50})$")]
         public string epost { get; set; }
-
+        [RegularExpression(@"^[0-9+]{2,20})$")]
         public string mobilnummer { get; set; }
+        [RegularExpression(@"^[a-zA-Z. \-]{2,20})$")]
         public string billettType { get; set; }
         public int reiseId { get; set; }
+        [RegularExpression(@"^[a-zA-Z. \-]{2,20})$")]
         public string reiseTil { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20})$")]
         public string reiseFra { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string datoAvreise { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string datoAnkomst { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string tidspunktFra { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string tidspunktTil { get; set; }
 
     }
--- WebApplication1/Models/Reise.cs
+++ WebApplication1/Models/Reise.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,17 @@ namespace WebApplication1.Modules
     public class Reise
     {
         public int id { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20})$")]
         public string reiseTil { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20})$")]
         public string reiseFra{ get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string tidspunktFra { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string tidspunktTil { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string datoAvreise { get; set; }
+        [RegularExpression(@"^[0-9./\:]{2,20})$")]
         public string datoAnkomst { get; set; }
     }
 }
