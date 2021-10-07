cc4b6660e5cad83cf5607c9e01860df87ae61696
PandersPanda <anderh0709@gmail.com>
2021-10-07 11:52:44 +0200

    Lagt tilbake regex i backend, som denne gangen funker

--- WebApplication1/Models/Billett.cs
+++ WebApplication1/Models/Billett.cs
@@ -10,29 +10,29 @@ namespace WebApplication1.Models
     public class Billett
     {
         public int id { get; set; }
-        //[RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string fornavn { get; set; }
-        //[RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,50}$")]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,50}$")]
         public string etternavn { get; set; }
-        //[RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ. \-@]{2,20}$")]
+        [RegularExpression(@"^[0-9a-zA-ZæøåÆØÅ. \-@]{2,20}$")]
         public string epost { get; set; }
-        //[RegularExpression(@"^[0-9+]{2,20}$")]
+        [RegularExpression(@"^[0-9+]{2,20}$")]
         public string mobilnummer { get; set; } 
         public int antallVoksne { get; set; }
         public int antallBarn { get; set; }
         public double totalPris { get; set; }
         public int reiseId { get; set; }
-        //[RegularExpression(@"^[a-zA-Z. \-]{2,20}$")]
+        [RegularExpression(@"^[a-zA-Z. \-]{2,20}$")]
         public string reiseTil { get; set; }
-        //[RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string reiseFra { get; set; }
-        //[RegularExpression(@"^[0-9./\:]{2,20}$")]
+        [RegularExpression(@"^[0-9./\:]{2,20}$")]
         public string datoAvreise { get; set; }
-        //[RegularExpression(@"^[0-9./\:]{2,20}$")]
+        [RegularExpression(@"^[0-9./\:]{2,20}$")]
         public string datoAnkomst { get; set; }
-        //[RegularExpression(@"^[0-9./\:]{2,20}$")]
+        [RegularExpression(@"^[0-9./\:]{2,20}$")]
         public string tidspunktFra { get; set; }
-        //[RegularExpression(@"^[0-9./\:]{2,20}$")]
+        [RegularExpression(@"^[0-9./\:]{2,20}$")]
         public string tidspunktTil { get; set; }
         public double reisePris { get; set; }
     }
