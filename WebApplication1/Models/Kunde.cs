500e8cfb505d8a89770778e04da4acd61475d5b6
PandersPanda <anderh0709@gmail.com>
2021-10-07 12:54:52 +0200

    Lagt til Kunde tabell og lagt til kortnummer og utløpsdato

---
+++ WebApplication1/Models/Kunde.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Kunde
+    {
+        public int id { get; set; }
+        public string fornavn { get; set; }
+        public string etternavn { get; set; }
+        public string epost { get; set; }
+        public string mobilnummer { get; set; }
+        public string kortnummer { get; set; }
+        public string utlopsdato { get; set; }
+    }
+}
