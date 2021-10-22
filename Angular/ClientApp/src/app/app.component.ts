58e3e97ac9fc1e59287d51da5b47959cbcf38d7c
AndreasTHansen <andreas-torres@hotmail.com>
2021-10-22 13:05:34 +0200

    Angular prosjekt satt opp, slettet nødvendige ting, og begynt på admin.html-en

--- Angular/ClientApp/src/app/app.component.ts
+++ Angular/ClientApp/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { kunde } from "kunde";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  public navn: string;
+  public telefon: string;
+
+  ngOnInit() {
+    this.navn = "Ole Hansen";
+    this.telefon = "92192122";
+  }
+
+  kunder: Array<kontakt>
 }
