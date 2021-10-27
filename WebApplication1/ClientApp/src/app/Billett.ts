d940e6de1e88d9d4913bd583cac6c883182a28d8
PandersPanda <anderh0709@gmail.com>
2021-10-27 12:56:03 +0200

    Lagt til Billett og reise klasser i angular

---
+++ WebApplication1/ClientApp/src/app/Billett.ts
@@ -0,0 +1,22 @@
+export class Billett {
+  id: number;
+  antallVoksne: number;
+  antallBarn: number;
+  totalPris: number;
+  kundeId: number;
+  fornavn: number;
+  etternavn: string;
+  mobilnummer: string;
+  epost: string;
+  kortnummer: string;
+  utlopsdato: string;
+  cvc: number;
+  reiseId: number;
+  reiseTil: string;
+  reiseFra: string;
+  tidspunktAvgang: string;
+  tidspunktAnkomst: string;
+  datoAvgang: string;
+  datoAnkomst: string;
+  reisePris: number;
+}
---
+++ WebApplication1/ClientApp/src/app/reise.ts
@@ -0,0 +1,10 @@
+export class Reise {
+  id: number;
+  reiseTil: string;
+  reiseFra: string;
+  tidspunktAvgang: string;
+  tidspunktAnkomst: string;
+  datoAvgang: string;
+  datoAnkomst: string;
+  reisePris: number;
+}
